b933f3d9795532a4fd147b76846b0baa34c27d4a
jribeiro <nicolas.jnr10@outlook.com>
2023-04-26 00:11:11 -0300

    feat(modal): cria modal com matDialog

--- src/app/components/continuation-register/continuation-register.component.ts
+++ src/app/components/continuation-register/continuation-register.component.ts
@@ -1,11 +1,10 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-continuation-register',
   templateUrl: './continuation-register.component.html',
   styleUrls: ['./continuation-register.component.scss']
 })
-export class ContinuationRegisterComponent {
+export class ContinuationRegisterComponent  {
+
 
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,51 +14,50 @@ export class LoginComponent implements OnInit {
   @ViewChild(PoModalComponent, { static: true }) modal!: PoModalComponent; // Obtenção do PoModalComponent usando ViewChild
 
 
-constructor(private fb: FormBuilder, private dialog: MatDialog){
+  constructor(private fb: FormBuilder, private dialog: MatDialog) { }
 
-}
+  formRegister!: FormGroup;
 
-formRegister!: FormGroup;
+  ngOnInit() {
+    this.initForms();
+    console.log(this.getValueControl(this.formRegister, 'name'))
+  }
 
+  getValueControl(form: FormGroup, control: string) {
+    return form.controls[control].value;
+  }
 
-ngOnInit(){
-this.initForms();
-console.log(this.getValueControl(this.formRegister, 'name'))
-}
+  initForms() {
+    this.formRegister = this.fb.group({
+      name: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      age: [null, Validators.required]
+    })
+  }
 
-debug(){
-  console.log(this.formRegister);
-}
+  openDialogRegister() {
+    this.dialog.open(ContinuationRegisterComponent, {
+      width: '600px',
+      autoFocus: false,
+      maxHeight: '90vh',
+      data: {
+        data: ''
+      }
 
-initForms(){
-  this.formRegister = this.fb.group({
-    name:[null, Validators.required],
-    email:[null, [Validators.required, Validators.email]],
-    age:[null, Validators.required]
-  })
-}
+    })
+  }
 
-openDialogRegister(){
-  this.createDataDialog();
-  this.modal.open()
-}
+  createDataDialog(
+    name = this.getValueControl(this.formRegister, 'name'),
+    email = this.getValueControl(this.formRegister, 'email'),
+    age = this.getValueControl(this.formRegister, 'age')) {
 
-createDataDialog(
-  name = this.getValueControl(this.formRegister, 'name'),
-  email = this.getValueControl(this.formRegister, 'email'),
-  age = this.getValueControl(this.formRegister, 'age')){
-  
     const dataDialog = {
       name,
       email,
       age
     }
-
     return dataDialog;
-}
-
-getValueControl(form: FormGroup, control: string){
-  return form.controls[control].value;
-}
+  }
 
 }
