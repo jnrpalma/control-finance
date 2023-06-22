5c877a8f5a99d4c28f7353d78fb9a5a5c86c68e2
jribeiro <nicolas.jnr10@outlook.com>
2023-06-22 17:51:53 -0300

    feat(loadin): logout e loadin adcionados

--- src/app/components/dashboard/menu/menu.component.ts
+++ src/app/components/dashboard/menu/menu.component.ts
@@ -3,21 +3,29 @@ import { LocalstorageService } from './../../../services/localstorage.service';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { DownloadImage } from 'src/app/interfaces/downloadimage';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
+
 export class MenuComponent implements OnInit {
 
   messageHour!: string;
   showNameUser!: string;
   isDefaultImage = '../../../../assets/images/default.png'
   imageUserUrl!: string;
+  isHideLoading = true;
 
-  constructor(private localStorageService: LocalstorageService, private apiService: ApiService, private sanitizer: DomSanitizer ) {}
-  
+  constructor(private localStorageService: LocalstorageService, 
+    private apiService: ApiService, private sanitizer: DomSanitizer,
+    private router: Router
+    
+    ) {}
   
   ngOnInit(): void {
     this.getNameUser();
@@ -38,6 +46,16 @@ export class MenuComponent implements OnInit {
   getNameUser() { 
     const nameUser = this.localStorageService.getLocalStorage('userInfo');
     this.showNameUser = nameUser.name;
+  }
+
+  logout() {
+    this.isHideLoading = false; // Exibe o componente de loading
+    this.localStorageService.removeLocalStorage('token');
+    // Simulação de um tempo de espera antes de redirecionar
+    setTimeout(() => {
+      this.isHideLoading = true; // Oculta o componente de loading
+      this.router.navigate(['/']);
+    }, 2000); // Tempo de espera de 2 segundos antes do redirecionamento
   }
 
 }
