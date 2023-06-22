73ad828efc1f5c71382201b012d5f4f837fd23a5
jribeiro <nicolas.jnr10@outlook.com>
2023-06-22 10:34:30 -0300

    feat(dasboard): cria dasboard e demais componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,10 +9,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { PoModalModule, PoModule } from '@po-ui/ng-components';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MenuComponent } from './components/dashboard/menu/menu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RevenuesCardComponent } from './components/dashboard/revenues-card/revenues-card.component';
+import { FooterComponent } from './components/dashboard/footer/footer.component';
+import { DebtsComponent } from './components/dashboard/debts/debts.component';
+import { RevenuesComponent } from './components/dashboard/revenues/revenues.component';
+import { BalanceTotalCardComponent } from './components/dashboard/balance-total-card/balance-total-card.component';
+import { DebtsCardComponent } from './components/dashboard/debts-card/debts-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    MenuComponent,
+    RevenuesCardComponent,
+    DebtsCardComponent,
+    BalanceTotalCardComponent,
+    RevenuesComponent,
+    DebtsComponent,
+    FooterComponent
+
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
     BrowserModule,
